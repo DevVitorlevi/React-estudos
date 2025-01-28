e815f7db934335d67d014f9e408832305c82227f
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-01-28 18:55:25 -0300

    COLOCABNDO UM NOVO PASSSO

--- fundamentos/src/components/Events.jsx
+++ fundamentos/src/components/Events.jsx
@@ -3,11 +3,24 @@ const Events = () => {
         document.body.style.background = "purple"
 
     }
+
+    const rendersomente = (x) => {
+        if (x) {
+            return <h1>True</h1>
+        } else {
+            return <h1>False</h1>
+        }
+
+    }
+
+
     return (
         <>
             <div>
                 <button onClick={Event}>Clique</button>
             </div>
+            {rendersomente(false)}
+            {rendersomente(true)}
         </>
     )
 
