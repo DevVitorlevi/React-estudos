ca597f93bca3224945e77bdd0169b29cd44e88be
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-01-29 00:34:10 -0300

    Previos State

--- avançado/src/components/ListRender.jsx
+++ avançado/src/components/ListRender.jsx
@@ -1,32 +1,47 @@
-import { useState } from "react"
+import { useState } from "react";
 
 const ListRender = () => {
+    const [list] = useState(["Levi", 'Kele', 'Jose', 'Maria']);
 
-    const [list] = useState(["Levi", 'Kele', 'Jose', 'Maria'])
-    const [users] = useState(
+    const [users, setUsers] = useState([
         { id: 12312, name: 'Vitor', age: 16 },
         { id: 32612, name: 'Maria', age: 48 },
         { id: 13, name: 'Simba', age: 1.7 }
-    )
+    ]);
+
+    const deleteUser = () => {
+        // Gera um índice aleatório com base no tamanho da lista de usuários
+        const randomIndex = Math.floor(Math.random() * users.length);
+
+        // Obtém o ID do usuário a ser removido
+        const userIdToRemove = users[randomIndex].id;
+
+        // Filtra a lista de usuários, removendo o usuário com o ID selecionado
+        setUsers((prevUsers) => {
+            return prevUsers.filter((user) => user.id !== userIdToRemove);
+        });
+    };
 
     return (
         <>
-
+            {/* Lista de nomes */}
             <ul>
-                {list.map((i) => (
-                    <li>{i}</li>
+                {list.map((item, index) => (
+                    <li key={index}>{item}</li>
                 ))}
             </ul>
 
-            <>
-                <ul>
-                    {users.map((user) => (
-                        <li key={user.id}>{user.name} - {user.age}</li>
-                    ))}
-                </ul>
-            </>
-        </>
-    )
-}
+            {/* Lista de usuários */}
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>{user.name} - {user.age}</li>
+                ))}
+            </ul>
 
-export default ListRender
+            {/* Botão para deletar um usuário aleatório */}
+            <button onClick={deleteUser}>Delete</button>
+        </>
+    );
+};
+
+export default ListRender;
