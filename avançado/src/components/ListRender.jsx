176268e04452a0eee313abc7d35866ada082841a
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-01-28 23:40:55 -0300

    key

--- avançado/src/components/ListRender.jsx
+++ avançado/src/components/ListRender.jsx
@@ -3,7 +3,11 @@ import { useState } from "react"
 const ListRender = () => {
 
     const [list] = useState(["Levi", 'Kele', 'Jose', 'Maria'])
-
+    const [users] = useState(
+        { id: 12312, name: 'Vitor', age: 16 },
+        { id: 32612, name: 'Maria', age: 48 },
+        { id: 13, name: 'Simba', age: 1.7 }
+    )
 
     return (
         <>
@@ -13,6 +17,14 @@ const ListRender = () => {
                     <li>{i}</li>
                 ))}
             </ul>
+
+            <>
+                <ul>
+                    {users.map((user) => (
+                        <li key={user.id}>{user.name} - {user.age}</li>
+                    ))}
+                </ul>
+            </>
         </>
     )
 }
