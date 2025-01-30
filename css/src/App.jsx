589c6ea90b3d8a98325f023c5a3795a66f2557fc
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-01-30 10:34:11 -0300

    Css Dinamico

---
+++ css/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './styles/App.css'
+
+function App() {
+  const n = 1
+
+  return (
+    <>
+      <h1 style={n > 12 ? { color: 'blue', backgroundColor: 'black' } : null}>Style Ternário</h1>
+
+      <div className="teste-app">
+        <p>Vitor</p>
+      </div>
+    </>
+  )
+}
+
+export default App
