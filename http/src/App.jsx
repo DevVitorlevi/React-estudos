1e4d9d9998a17d5b352ab3118e8957f9ce20f848
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-01-31 00:08:19 -0300

    Resgatando dados da  API

--- http/src/App.jsx
+++ http/src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+// Define a URL da API onde os dados dos produtos estão localizados
+const url = 'http://localhost:3000/products';
 
+// Define o componente funcional App
 function App() {
-  const [count, setCount] = useState(0)
 
+  // Cria um estado chamado 'products' e uma função 'setProduct' para atualizá-lo
+  // O estado inicial é um array vazio
+  const [products, setProduct] = useState([]);
+
+  // Usa o hook useEffect para executar um efeito colateral (buscar dados da API)
+  // O efeito será executado apenas uma vez, quando o componente for montado
+  useEffect(() => {
+    // Define uma função assíncrona chamada fetchData para buscar os dados da API
+    async function fetchData() {
+      // Faz uma requisição GET para a URL da API e espera a resposta
+      const res = await fetch(url);
+
+      // Converte a resposta em formato JSON e espera o resultado
+      const data = await res.json();
+
+      // Atualiza o estado 'products' com os dados recebidos da API
+      setProduct(data);
+    }
+
+    // Chama a função fetchData para executar a busca dos dados
+    fetchData();
+  }, []); // O array vazio [] garante que o useEffect só será executado uma vez
+
+  // Retorna o JSX que será renderizado na tela
   return (
+
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <ul>
+        {products.map((product) => (
+          // Cada item da lista tem uma chave única (key) para ajudar o React na renderização
+          <li key={product.id}>
+            {/* Exibe o nome e o preço do produto */}
+            {product.name} - R$:{product.price}
+          </li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
 export default App
