55311aa84df4cd598b92bf51855aab940d202e17
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-02-02 00:23:07 -0300

    NavLink

--- react-router/src/components/Header.jsx
+++ react-router/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom"
 import style from '../styles/Header.module.css'
+import { NavLink } from "react-router-dom"
 
 
 const Header = () => {
@@ -8,8 +9,8 @@ const Header = () => {
             <h1>React Router</h1>
             <nav id={style.nav}>
                 <ul id={style.list}>
-                    <li className={style.item}><Link to='/main' className={style.a}>Home</Link></li>
-                    <li className={style.item}><Link to='/about' className={style.a}>About</Link></li>
+                    <li className={style.item}><NavLink to='/main' className={({ isActive }) => isActive ? `${style.a} ${style.active}` : style.a}>Home</NavLink></li>
+                    <li className={style.item}><NavLink to='/about' className={({ isActive }) => isActive ? `${style.a} ${style.active}` : style.a}>About</NavLink></li>
                 </ul>
             </nav>
         </header>
